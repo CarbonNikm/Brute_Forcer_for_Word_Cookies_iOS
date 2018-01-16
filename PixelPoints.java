736a80baa23dc875689405e85ab5a80e7ecfb56e
CarbonNikm <33556831+CarbonNikm@users.noreply.github.com>
2018-01-16 13:39:36 -0800

    Create PixelPoints.java

---
+++ PixelPoints.java
@@ -0,0 +1,10 @@
+package luapermuations;
+
+public class PixelPoints {
+    public int x;
+    public int y;
+    public PixelPoints(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
